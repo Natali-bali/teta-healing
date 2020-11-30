bd2a6cda7067bbbc8226522d059636d4e192ef65
Natalia Konovalova <ko.natalia.de@gmail.com>
2020-11-30 14:01:29 +0100

    added styles for phones screen 768px

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let myFunction = function() {
+  console.log('works');
     document.querySelector('.menu-icon__lines').classList.toggle('menu-icon__cross');
     document.querySelector('#navbar').classList.toggle('mobile-menu__active');
 }
 let menuIcon = document.querySelector('#menu-icon');
-  menuIcon.onclick =  myFunction;
+  menuIcon.onclick = myFunction;
